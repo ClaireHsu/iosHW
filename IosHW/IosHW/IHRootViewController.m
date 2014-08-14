23a5ea442cddc7e5d88ea8498529924e4b7d7826
ClaireHsu <claire.hsu@livebricks.com>
2014-08-14 10:22:47 +0800

    ttt
    t

--- IosHW/IosHW/IHRootViewController.m
+++ IosHW/IosHW/IHRootViewController.m
@@ -27,6 +27,47 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    fullScreen = [[UIScreen mainScreen] bounds];
+    self.scrollView = [[UIScrollView alloc]initWithFrame:fullScreen];
+    self.scrollView.backgroundColor = [UIColor greenColor];
+    self.scrollView.delegate = self;
+    [self.scrollView setPagingEnabled:YES];
+    self.scrollView.contentSize = CGSizeMake(fullScreen.size.width*3, fullScreen.size.height);
+    [self.view addSubview:self.scrollView];
+    
+    int defaultPageControlHeight = 36;
+    self.pageControl = [[UIPageControl alloc]initWithFrame:CGRectMake(0, fullScreen.size.height-defaultPageControlHeight, fullScreen.size.width, defaultPageControlHeight)];
+    self.pageControl.numberOfPages = 3;
+    self.pageControl.pageIndicatorTintColor = [UIColor grayColor];
+    self.pageControl.currentPageIndicatorTintColor = [UIColor redColor];
+    [self.view addSubview:self.pageControl];
+    
+    
+    UIImageView *imgView = [[UIImageView alloc]
+                            initWithFrame:CGRectMake(0, 0, fullScreen.size.width, 200)];
+    imgView.image = [UIImage imageNamed:@"asset@2x.png"];
+    [self.scrollView addSubview:imgView];
+    
+    
+    UITextView *textView = [[UITextView alloc]initWithFrame:CGRectMake(0, 200, fullScreen.size.width, fullScreen.size.height-200)];
+    textView.text = @"description";
+    textView.backgroundColor = [UIColor blueColor];
+    textView.editable = NO;
+    [self.scrollView addSubview:textView];
+
+    
+    
+    
+}
+
+#pragma mark - UIScrollView delegate
+-(void) scrollViewDidScroll:(UIScrollView *)scrollView{
+    CGFloat pageWidth = self.scrollView.frame.size.width;
+    int page = floor((self.scrollView.contentOffset.x-pageWidth/2)/pageWidth)+1;
+    //NSLog(@"page number:%d",page);
+    self.pageControl.currentPage = page;
+
 }
 
 - (void)didReceiveMemoryWarning
